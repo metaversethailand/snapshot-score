690ad126e35f5b89ef75d8b086fd804ec557744b
Dome C <dome@tel.co.th>
2021-11-21 22:41:33 +0700

    first commit

---
+++ src/api.ts
@@ -0,0 +1,65 @@
+import express from 'express';
+import snapshot from '@snapshot-labs/strategies';
+import scores, { blockNumByNetwork } from './scores';
+import { clone } from './utils';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json(blockNumByNetwork);
+});
+
+router.get('/strategies', (req, res) => {
+  const strategies = Object.fromEntries(
+    Object.entries(clone(snapshot.strategies)).map(([key, strategy]) => [
+      key,
+      // @ts-ignore
+      { key, ...strategy }
+    ])
+  );
+  res.json(strategies);
+});
+
+router.post('/scores', async (req, res) => {
+  const { params } = req.body;
+  const { space = '', network, snapshot = 'latest', strategies, addresses } = params;
+
+  if (strategies.map(strategy => strategy.name).includes('pod-leader'))
+    return res.status(500).json({
+      jsonrpc: '2.0',
+      error: {
+        code: 500,
+        data: 'something wrong with the strategies'
+      }
+    });
+
+  let result;
+  try {
+    result = await scores(
+      {},
+      {
+        space,
+        network,
+        snapshot,
+        strategies,
+        addresses
+      }
+    );
+  } catch (e) {
+    console.log('Get scores failed', space, e);
+    return res.status(500).json({
+      jsonrpc: '2.0',
+      error: {
+        code: 500,
+        data: e
+      }
+    });
+  }
+
+  return res.json({
+    jsonrpc: '2.0',
+    result
+  });
+});
+
+export default router;
---
+++ src/scores.ts
@@ -0,0 +1,60 @@
+import { createHash } from 'crypto';
+import snapshot from '@snapshot-labs/strategies';
+import { get, set } from './aws';
+
+export const blockNumByNetwork = {};
+const blockNumByNetworkTs = {};
+const delay = 30;
+
+async function getBlockNum(network) {
+  const ts = parseInt((Date.now() / 1e3).toFixed());
+  if (blockNumByNetwork[network] && blockNumByNetworkTs[network] > ts - delay) return blockNumByNetwork[network];
+
+  const provider = snapshot.utils.getProvider(network);
+  const blockNum = await provider.getBlockNumber();
+
+  blockNumByNetwork[network] = blockNum;
+  blockNumByNetworkTs[network] = ts;
+
+  return blockNum;
+}
+
+export default async function scores(parent, args) {
+  const { space = '', strategies, network, addresses } = args;
+
+  const key = createHash('sha256')
+    .update(JSON.stringify(args))
+    .digest('hex');
+  console.log('Key', key, JSON.stringify({ space, strategies, network }), addresses.length);
+
+  let snapshotBlockNum = 'latest';
+  if (args.snapshot !== 'latest') {
+    const currentBlockNum = await getBlockNum(network);
+    snapshotBlockNum = currentBlockNum < args.snapshot ? 'latest' : args.snapshot;
+  }
+
+  const state = snapshotBlockNum === 'latest' ? 'pending' : 'final';
+  let scores;
+
+  if (state === 'final') scores = await get(key);
+
+  if (!scores) {
+    scores = await snapshot.utils.getScoresDirect(
+      space,
+      strategies,
+      network,
+      snapshot.utils.getProvider(network),
+      addresses,
+      snapshotBlockNum
+    );
+
+    if (state === 'final') {
+      set(key, scores).then(() => console.log('Stored!'));
+    }
+  }
+
+  return {
+    state,
+    scores
+  };
+}
